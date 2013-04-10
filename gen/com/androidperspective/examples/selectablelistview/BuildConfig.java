072cb0cb3340f87be1811f1c37eed930c40148f8
deniskratinov <denis.kratinov@gmail.com>
2013-04-10 10:36:39 +0300

    cleaning repo

--- gen/com/androidperspective/examples/selectablelistview/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.androidperspective.examples.selectablelistview;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
